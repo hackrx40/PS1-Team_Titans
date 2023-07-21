bfd6cf40b41ef2d27b6fe0c3daf4febaab0c0ae6
Abhijit <abhijit.panda1319@gmail.com>
2023-07-21 20:44:59 +0530

    Configured module to get all chat history and conversations

--- hackrx-healthbot-server/controllers/chat.js
+++ hackrx-healthbot-server/controllers/chat.js
@@ -1,6 +1,14 @@
 const Query = require("../models/Query");
 const { v4: uuid } = require("uuid");
 
+const getChatWithID = async(id) => {
+  try {
+    const query = await Query.findOne({ _id: id });
+    return query
+  } catch (err) {
+    return "Error"
+  }
+}
 
 exports.chat = async (req, res) => {
   try {
@@ -21,6 +29,19 @@ exports.chat = async (req, res) => {
       message: data.choices[0]?.text.trim(),
       _id: data.choices[0] ? tempId : undefined,
     });
+  } catch (err) {
+    res.status(400).send({ success: false, message: err.message });
+  }
+};
+
+exports.getAllChats = async (req, res) => {
+  try {
+    const query = await Query.findOne({ _id: req.queryId });
+    if (!query)
+      return res
+        .status(400)
+        .send({ success: false, message: "Query doesn't exist" });
+    res.send(query);
   } catch (err) {
     res.status(400).send({ success: false, message: err.message });
   }
