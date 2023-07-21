7868d34e687e74160668d91a3ee5e5ff5c6fdb5a
Abhijit <abhijit.panda1319@gmail.com>
2023-07-21 20:15:26 +0530

    Login setup for auth via Mongo

--- hackrx-healthbot-server/controllers/auth.js
+++ hackrx-healthbot-server/controllers/auth.js
@@ -22,3 +22,23 @@ exports.signup = async (req, res) => {
       res.status(400).send({ success: false, message: err.message });
     }
   };
+
+exports.login = async (req, res) => {
+    try {
+      const findUser = await User.findOne({ username: req.body.username });
+      if (!findUser)
+        return res
+          .status(401)
+          .send({ success: false, message: "User doesn't exist" });
+      const passwordMatch = compareSync(req.body.password, findUser.password);
+      if (!passwordMatch)
+        return res
+          .status(401)
+          .send({ success: false, message: "Wrong password" });
+      findUser.password = undefined;
+      res.send(findUser);
+    } catch (err) {
+      res.status(400).send({ error: err.message });
+    }
+  };
+  
