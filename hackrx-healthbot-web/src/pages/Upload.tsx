958a6b4b1efc6b8126d98e9cb17a0b489773c0d6
Abhas-15 <abhirup.abhas@gmail.com>
2023-07-22 02:35:19 +0530

    feat:add xray

--- hackrx-healthbot-web/src/pages/Upload.tsx
+++ hackrx-healthbot-web/src/pages/Upload.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import bg from "../assets/xray_land.jpg";
 
 const buttonStyle = {
   backgroundColor: "#4caf50",
@@ -7,8 +9,9 @@ const buttonStyle = {
   border: "none",
   borderRadius: "4px",
   cursor: "pointer",
-  margin: "0 10px",
+  margin: "10px",
 };
+
 const buttonStyle1 = {
   backgroundColor: "#FF0000",
   color: "white",
@@ -16,118 +19,131 @@ const buttonStyle1 = {
   border: "none",
   borderRadius: "4px",
   cursor: "pointer",
-  margin: "0 10px",
+  margin: "10px",
 };
 
 const UploadPage = () => {
-  const [selectedImage, setSelectedImage] =  useState<any>(null);
+  const [selectedImage, setSelectedImage] = useState<any>(null);
   const [imagePreview, setImagePreview] = useState<any>(null);
-  const [diagnosis, setDiagnosis] = useState(null);
+  const [diagnosis, setDiagnosis] = useState<any>(null);
+  const [analyzing, setAnalyzing] = useState<boolean>(false);
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedImage(file);
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.size <= 5 * 1024 * 1024) {
+      setSelectedImage(file);
 
-    // Create a preview URL for displaying the selected image
-    const reader = new FileReader();
-    reader.onload = () => {
-      setImagePreview(reader.result);
-    };
-    reader.readAsDataURL(file);
+      // Create a preview URL for displaying the selected image
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Please select an image with size less than or equal to 5MB.");
+    }
   };
-  const handleSubmit=async(e:any)=>{
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    let formData=new FormData();
-    formData.append("file", selectedImage);
-    const response = await fetch("http://localhost:8000/uploadimage", {
-      method: "POST",
-      body: formData,
-    });
-    const responseWithBody = await response.json();
-    if (response) setDiagnosis(responseWithBody);
-   
-  }
-  console.log(diagnosis);
+    if (selectedImage) {
+      setAnalyzing(true);
+      let formData = new FormData();
+      formData.append("file", selectedImage);
+      const response = await fetch("http://localhost:8000/uploadimage", {
+        method: "POST",
+        body: formData,
+      });
+      const responseWithBody = await response.json();
+      setAnalyzing(false);
+      setDiagnosis(responseWithBody);
+      // setImagePreview(null); // Clear image preview after analysis
+    }
+  };
 
   const handleReUpload = () => {
     setSelectedImage(null);
     setImagePreview(null);
+    setDiagnosis(null);
   };
 
+  const DiagnosisContent = () => {
+    if (!diagnosis || !diagnosis.content) return null;
+
+    // Split the diagnosis content into an array of lines based on "\n"
+    const lines = diagnosis.content.split("\n");
+
+    return (
+     <div
+        style={{
+    marginTop:"20px",
+    marginBottom: "20px",
+    marginLeft: "100px",
+    border: "2px solid #ddd",
+    padding: "30px",
+    width: "80%",
+    textAlign: "left",
+    lineHeight: "30px",
+    fontSize:"16px"
+  }}
+>
+  {lines.map((line, index) => (
+    <p key={index}>
+      {index % 2 === 0 && <span style={{ marginRight: "10px" }}>{index/2 + 1}.</span>}
+      {line}
+    </p>
+))}
+
+</div>
+
+    );
+  };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", textAlign: "center" }}>
-      <h1 style={{ marginBottom: "20px" }}>Upload an X-Ray Report</h1>
-      {!selectedImage ? (
-        <div style={{ marginBottom: "20px" }}>
-          <input type="file" name="file" accept="image/*" onChange={handleImageChange} />
-        </div>
-      ) : (
-        <div>
-          <img src={imagePreview} alt="Selected" style={{ maxWidth: "100%", height: "auto", marginBottom: "20px" }} />
-          <div>
-            <button style={buttonStyle1} onClick={handleReUpload}>Re-Upload</button>
-            <button style={buttonStyle} onClick={handleSubmit}>Confirm</button>
+    <>
+      <Navbar />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundImage: `url(${bg})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+        }}
+      >
+        <h1 style={{ marginTop: "18vh", marginBottom: "5vh" }}>Hello, I am BAYMAX, your AI-powered Healthcare Companion!</h1>
+        <h2 style={{ marginBottom: "5vh" }}>Let's embark on a journey of proactive well-being together!</h2>
+
+        {!selectedImage ? (
+          <div style={{ marginBottom: "20px", border: "2px solid #ddd", padding: "40px", width: "80%", textAlign: "center", fontSize:"20px" }}>
+            <p>Please upload an X-Ray report image (max 5MB):</p>
+            <input type="file" name="file" accept="image/*" onChange={handleImageChange} />
           </div>
-        </div>
-      )}
-    </div>
+        ) : analyzing ? (
+          <div style={{ marginBottom: "20px", border: "2px solid #ddd", padding: "10px", width: "80%", textAlign: "center" }}>
+            <h2>Analyzing...</h2>
+          </div>
+        ) : (
+          <div style={{ width: "80%", textAlign: "center" }}>
+            <img src={imagePreview} alt="Selected" style={{ maxWidth: "100%", maxHeight: "500px", marginBottom: "20px" }} />
+            {DiagnosisContent()}
+            {diagnosis ? (
+              <div>
+                <button style={buttonStyle1} onClick={handleReUpload}>Re-Upload</button>
+              </div>
+            ) : (
+              <div>
+                <button style={buttonStyle1} onClick={handleReUpload}>Re-Upload</button>
+                <button style={buttonStyle} onClick={handleSubmit}>Confirm</button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
 export default UploadPage;
-
-// width: "50%",
-// margin: "auto",
-// height: "100vh",
-// position: "relative",
-// display: "flex",
-// }}
-// >
-// <div style={{
-//   flex: "50%",
-// }}>
-//   {messages.length != 0 ? (
-//   <div
-//       className="inner_cont"
-//           style={{
-//           height: "88%",
-//           overflowY: "scroll",
-//           }}
-//       >
-//           <Messages show={loading} messages={messages} />
-//   </div>
-//   ) : (
-//       <Hero />
-//   )}
-
-//   <Input
-//       chatLoading={chatLoading}
-//       handlelogout={handlelogout}
-//       img={auth?.avatar}
-//       apiKey={auth?.apiKey}
-//       toggleLoading={toggleLoading}
-//       addMessage={addMessage}
-//   />
-// </div>
-// <div style={{
-//   flex: "50%",
-// }}>
-//   {!diagnosis
-//   ?
-//   <form
-//       onSubmit={handleSubmit}
-//       style={{display:"grid", gridRowGap:"20px"}}
-//   >
-//       <input type="file" name="file" onChange={handleFileChange}></input>
-//       <button type="submit">Upload</button>
-//   </form>
-//   :
-//   <div>
-//       {diagnosis.content}
-//   </div>
-//   }
-  
-
-// </div>
-// </div>
