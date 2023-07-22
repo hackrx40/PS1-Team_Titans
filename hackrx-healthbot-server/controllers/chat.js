af06580b98684c7e7362bf9c5d16c4698a894cfe
revan <revanth200218@gmail.com>
2023-07-22 09:31:15 +0530

    Server chat changes

--- hackrx-healthbot-server/controllers/chat.js
+++ hackrx-healthbot-server/controllers/chat.js
@@ -1,33 +1,36 @@
 const Query = require("../models/Query");
 const { v4: uuid } = require("uuid");
-
-const getChatWithID = async(id) => {
-  try {
-    const query = await Query.findOne({ _id: id });
-    return query
-  } catch (err) {
-    return "Error"
-  }
-}
+const dotenv = require("dotenv")
+dotenv.config()
+const axios = require('axios')
 
 exports.chat = async (req, res) => {
   try {
     const tempId = uuid();
-    
-    let oldConvo = getChatWithID(req.queryId)
-    oldConvo.push(message);
-    
+    await Query.updateOne(
+      { _id: req.queryId },
+      { $push: { texts: { message: req.body.message, textBy: 1 } } }
+    );
+    // const { data } = await createChatGTP({
+    //   message: req.body.message,
+    // });
+
     const data = {
       prompt : req.body.message,
+      algo : req.body.algo
     };
     console.log("Generating response to your message...", data);
 
-    const response = await axios.post('url/generate', data)
+    const response = await axios.post('http://127.0.0.1:8000/generate', data)
     console.log("Response Generated", response.data)
-
+    await Query.updateOne(
+      { _id: req.queryId },
+      { $push: { texts: { message: response.data.response, textBy: 0 }, }, }
+    );
+    
     res.send({
-      message: data.choices[0]?.text.trim(),
-      _id: data.choices[0] ? tempId : undefined,
+      message: response.data.response,
+      _id: response.data.response ? tempId : undefined,
     });
   } catch (err) {
     res.status(400).send({ success: false, message: err.message });
